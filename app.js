45650c63fe7d59cb4206292bfb318895ffa6f78d
Elena roncevic <elena@MacBook-Air.local>
2024-10-22 22:51:15 +0200

    izmena za cao neno

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const pgp = require('pg-promise')();
+const PORT = 3000;
+
+const db = pgp('postgres://username:password@localhost:5432/moj-projekat');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('cao, neno!');
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
